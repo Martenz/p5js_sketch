75c7c44b7f51d427f3f0a5dff9829d87f52186b9
Tenz <Martenz@users.noreply.github.com>
2017-01-05 16:26:23 +0100

    Update sketch.js

--- p5_empty/sketch.js
+++ p5_empty/sketch.js
@@ -2,15 +2,26 @@
 // http://codingrainbow.com
 // http://patreon.com/codingrainbow
 
+var ship;
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
+  ship = new Ship();
 }
 
 function draw() {
   background(0);
 
 
+}
+
+function Ship() {
+  this.pos = createVector(widt/2, height/2);
+  this.r = 10;
+  this.render = function() {
+    triangle(-this.r,this.r,this.r, this.r,0,-this.r);
+    
+  }
 }
 
 function keyPressed() {
