f3ea3884a5625a43d52bcc52ddbbdf4b7584bba8
Tenz <Martenz@users.noreply.github.com>
2017-01-05 17:45:10 +0100

    Update asteroid.js

--- p5_empty/asteroid.js
+++ p5_empty/asteroid.js
@@ -2,7 +2,9 @@ function Asteroid(){
  this.pos = createVector(random(width), random(height));
  this.r = 50; 
   this.render = function() {
+   push();
    translate(this.pos.x, this.pos.y);
-    ellipse(0,0, this.r*2 );
+   ellipse(0,0, this.r*2 );
+   pop();
   }
 }
