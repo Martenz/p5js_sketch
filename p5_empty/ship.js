bf0d5f6516caff26923d18de567b02c474394615
Tenz <Martenz@users.noreply.github.com>
2017-01-05 18:33:54 +0100

    Update ship.js

--- p5_empty/ship.js
+++ p5_empty/ship.js
@@ -33,7 +33,7 @@ function Ship() {
     noFill();
     stroke(255);
     triangle(-this.r,this.r,this.r, this.r,0,-this.r);
-    image(this.img, -this.r/2, this.r/2);
+    image(this.img, -this.r, this.r);
     pop();
   }
   
