458eed21782d8ff198d8542eb0b840dea0cbbc21
Tenz <Martenz@users.noreply.github.com>
2017-01-05 16:54:52 +0100

    Update sketch.js

--- p5_empty/sketch.js
+++ p5_empty/sketch.js
@@ -42,7 +42,7 @@ function Ship() {
     translate(this.pos.x,this.pos.y);
     rotate(this.heading);
     noFill();
-    stroke(155);
+    stroke(255);
     triangle(-this.r,this.r,this.r, this.r,0,-this.r);
   }
   
