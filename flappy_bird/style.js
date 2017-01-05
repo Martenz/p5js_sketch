2dbf6df5a8f151d4d94dd11197e0b83266f154dc
Tenz <Martenz@users.noreply.github.com>
2017-01-05 13:36:00 +0100

    Update style.js

--- flappy_bird/style.js
+++ flappy_bird/style.js
@@ -3,3 +3,8 @@ $=jQuery;
 $( document ).ready(function() {
   $('body').css('margin','0px');
 });
+
+$( window ).resize(function() {
+  $('canvas').css('width','100%');
+  $('canvas').css('height','100%');
+});
