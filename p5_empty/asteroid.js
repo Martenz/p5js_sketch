c0e5d1d90c714a65b3818e2e88ec0ae4d3cac323
Tenz <Martenz@users.noreply.github.com>
2017-01-05 18:12:57 +0100

    Update asteroid.js

--- p5_empty/asteroid.js
+++ p5_empty/asteroid.js
@@ -26,7 +26,20 @@ function Asteroid(){
      vertex(x,y);
    }
    endShape(CLOSE);
-   
    pop();
   }
+ 
+  this.edges = function() {
+    if (this.pos.x > width + this.r) {
+      this.pos.x = -this.r;
+    }else if (this.pos.x < -this.r) {
+      this.pos.x = width + this.r;
+    }
+    if (this.pos.y > height + this.r) {
+      this.pos.y = -this.r;
+    }else if (this.pos.y < -this.r) {
+      this.pos.y = height + this.r;
+    }
+  }
+ 
 }
