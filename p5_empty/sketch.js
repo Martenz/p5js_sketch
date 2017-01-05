86b16881b6f3772ff3eb6302571225e1c9baa210
Tenz <Martenz@users.noreply.github.com>
2017-01-05 16:09:16 +0100

    Add files via upload

---
+++ p5_empty/sketch.js
@@ -0,0 +1,21 @@
+// Daniel Shiffman
+// http://codingrainbow.com
+// http://patreon.com/codingrainbow
+
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+}
+
+function draw() {
+  background(0);
+
+
+}
+
+function keyPressed() {
+  if (key == ' ') {
+    //console.log("SPACE");
+  }
+  
+}
---
+++ p5_empty/style.js
@@ -0,0 +1,10 @@
+$=jQuery;
+
+$( document ).ready(function() {
+  $('body').css('margin','0px');
+});
+
+$( window ).resize(function() {
+  $('canvas').css('width','100%');
+  $('canvas').css('height','100%');
+});
