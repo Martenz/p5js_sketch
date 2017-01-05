8dc4061cfba7f3a158fa4ffcbac490114816d501
Tenz <Martenz@users.noreply.github.com>
2017-01-05 16:27:16 +0100

    Update sketch.js

--- p5_empty/sketch.js
+++ p5_empty/sketch.js
@@ -16,7 +16,7 @@ function draw() {
 }
 
 function Ship() {
-  this.pos = createVector(widt/2, height/2);
+  this.pos = createVector(width/2, height/2);
   this.r = 10;
   this.render = function() {
     triangle(-this.r,this.r,this.r, this.r,0,-this.r);
