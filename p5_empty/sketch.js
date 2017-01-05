55d6e8be6e6b78f99a303bb760d64900568b10bf
Tenz <Martenz@users.noreply.github.com>
2017-01-05 17:29:38 +0100

    Update sketch.js

--- p5_empty/sketch.js
+++ p5_empty/sketch.js
@@ -75,12 +75,12 @@ function Ship() {
   this.edges = function() {
     if (this.pos.x > width + this.r) {
       this.pos.x = -this.r;
-    }elseif (this.pos.x < -this.r) {
+    }else if (this.pos.x < -this.r) {
       this.pos.x = width + this.r;
     }
     if (this.pos.y > height + this.r) {
       this.pos.y = -this.r;
-    }elseif (this.pos.y < -this.r) {
+    }else if (this.pos.y < -this.r) {
       this.pos.y = height + this.r;
     }
   }
