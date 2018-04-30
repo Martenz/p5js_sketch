a13e6fb0d3a5bc547b154350aa4f41ef30fee083
Tenz <Martenz@users.noreply.github.com>
2018-04-30 16:44:34 +0200

    Add files via upload

---
+++ leaflet_geojson/sample-geojson.js
@@ -0,0 +1,33 @@
+var map = L.map('map').setView([12, 42], 2);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+  maxZoom: 18,
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  id: 'mapbox.light'
+}).addTo(map);
+
+//GeoJson Static example
+var geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }
+};
+
+L.geoJSON(geojsonFeature).addTo(map);
+
+// geojson api example
+var geojson_api = "https://www.mapbox.com/help/data/stations.geojson";
+$.getJSON( geojson_api, function( data ) {
+  console.log(data);
+  L.geoJSON(data).addTo(map);
+
+});
